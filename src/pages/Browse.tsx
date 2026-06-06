8bf03126554293cdb89f40ef59992489ec25599d
bbznho2-lang <bbznho2@gmail.com>
2026-06-06 02:20:53 -0400

    Update Browse.tsx

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -347,10 +347,9 @@ const Browse = () => {
                 <div className="flex items-center gap-2 px-3 pb-1">
                   {[
                     { label: "Instagram", Icon: Instagram, href: "https://instagram.com/queerscenes" },
-                    { label: "TikTok", Icon: Music2, href: "https://tiktok.com/@queerscenes" },
+                    { label: "TikTok", Icon: Music2, href: "https://www.tiktok.com/@queer.scenes" },
                     { label: "X / Twitter", Icon: Twitter, href: "https://twitter.com/queerscenes" },
-                    { label: "YouTube", Icon: Youtube, href: "https://youtube.com/@queerscenes" },
-                    { label: "Facebook", Icon: Facebook, href: "https://facebook.com/queerscenes" },
+                    { label: "YouTube", Icon: Youtube, href: "https://youtube.com/@queerscenestv?si=8mlUwn2WVYa-g-e5" },
                   ].map(({ label, Icon, href }) => (
                     <a
                       key={label}
